9d0e88e21f37726e39489c87b9783e149fa49b93
fjc15861094055 <2580164381@qq.com>
2023-04-09 00:02:39 +0800

    story:8 测试一次

---
+++ lcstudy/src/com/fjc/demo/example/Month3.java
@@ -0,0 +1,15 @@
+package com.fjc.demo.example;
+
+import java.util.ArrayList;
+
+/**
+ * author 凡
+ * @author Administrator
+ */
+public class Month3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> obj = new ArrayList<>();
+        obj.add(1);
+
+    }
+}
