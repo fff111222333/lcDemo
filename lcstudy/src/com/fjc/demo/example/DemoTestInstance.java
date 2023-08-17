b8a780434cc96ed2d36691e51f4bf5852022a64f
fjc15861094055 <2580164381@qq.com>
2023-08-17 14:49:10 +0800

    story:10 持证管理中证件逾期提醒的优化过程

---
+++ lcstudy/src/com/fjc/demo/example/DemoTestInstance.java
@@ -0,0 +1,21 @@
+package com.fjc.demo.example;
+
+/**
+ * author 凡
+ */
+public class DemoTestInstance {
+    public static String toStr(Object value, String defaultValue){
+        if(null == value){
+            return defaultValue;
+        }
+        if(value instanceof String){
+            return (String)value;
+        }
+        return value.toString();
+    }
+    public static void main(String[] args) {
+        Object value = 100;
+        System.out.println(toStr(value,"10000"));
+        System.out.println(10);
+    }
+}
